af66ffc900c249d640b2c977cf0f0dcc9f6d71d5
Francisco8NGY <franciscoIsco8@gmail.com>
2021-09-06 12:06:03 -0500

    template

---
+++ resources/views/components/card-icons.blade.php
@@ -0,0 +1,8 @@
+
+    <div class="w-full md:w-1/2 xl:w-1/3 p-3">
+        <div class="bg-white border rounded shadow p-2">
+            <div class="flex flex-row items-center">
+                {{$slot}}
+            </div>
+        </div>
+    </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,46 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<x-template-layout>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+    
+            <div class="flex flex-wrap">  
+                <x-card-icons>
+                    <div class="flex-shrink pr-4">
+                        <div class="rounded p-3 bg-green-600"><i class="fa fa-wallet fa-2x fa-fw fa-inverse"></i></div>
+                    </div>
+                    <div class="flex-1 text-right md:text-center">
+                        <h5 class="font-bold uppercase text-gray-500">Total Revenue</h5>
+                        <h3 class="font-bold text-3xl">$3249 <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3>
+                    </div>
+                </x-card-icons>
+
+                <x-card-icons>
+                    <div class="flex-shrink pr-4">
+                        <div class="rounded p-3 bg-pink-600"><i class="fas fa-users fa-2x fa-fw fa-inverse"></i></div>
+                    </div>
+                    <div class="flex-1 text-right md:text-center">
+                        <h5 class="font-bold uppercase text-gray-500">Total Users</h5>
+                        <h3 class="font-bold text-3xl">249 <span class="text-pink-500"><i class="fas fa-exchange-alt"></i></span></h3>
+                    </div>
+                </x-card-icons>
+
+                <x-card-icons>
+                    <div class="flex-shrink pr-4">
+                        <div class="rounded p-3 bg-pink-600"><i class="fas fa-users fa-2x fa-fw fa-inverse"></i></div>
+                    </div>
+                    <div class="flex-1 text-right md:text-center">
+                        <h5 class="font-bold uppercase text-gray-500">Total Users</h5>
+                        <h3 class="font-bold text-3xl">249 <span class="text-pink-500"><i class="fas fa-exchange-alt"></i></span></h3>
+                    </div>
+                </x-card-icons>
+
+               
             </div>
-        </div>
-    </div>
-</x-app-layout>
+
+            <hr class="border-b-2 border-gray-400 my-8 mx-4">
+    
+            
+        <h1>Hola</h1>
+
+    
+
+    
+</x-template-layout>
